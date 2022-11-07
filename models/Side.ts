77ac8192f1aaff09b3df23cf46fd8f0e63d62e66
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2022-11-07 19:34:02 +0700

    TokenDemoPage

---
+++ models/Side.ts
@@ -0,0 +1,19 @@
+import { z } from 'zod'
+import { getArraySchema } from 'zenbox-util/zod'
+import { identity } from 'lodash-es'
+
+export const SideSchema = z.enum(['Buy', 'Sell']).describe('Side')
+
+export const SidesSchema = getArraySchema(SideSchema, identity)
+
+export type Side = z.infer<typeof SideSchema>
+
+export function parseSide(side: Side): Side {
+  return SideSchema.parse(side)
+}
+
+export function parseSides(sides: Side[]): Side[] {
+  return SidesSchema.parse(sides)
+}
+
+export const isEqualSide = (a: Side) => (b: Side) => a === b
