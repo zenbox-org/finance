e299cf79003d5b188f7465714df79eaedbb082bb
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2022-12-28 15:34:38 +0700

    Upgrade all

--- models/Transfer/fromTransfersToAddresses.ts
+++ models/Transfer/fromTransfersToAddresses.ts
@@ -1,7 +1,8 @@
 import { flatten, uniq } from 'lodash-es'
-import { Address } from '../../../ethereum/models/Address'
+import { Address, AddressSchema } from '../../../ethereum/models/Address'
 import { Transfer } from '../Transfer'
 
 export function fromTransfersToAddresses(transfers: Transfer[]) {
-  return uniq<Address>(flatten(transfers.map(t => ([t.from.name, t.to.name]))))
+  const names = flatten(transfers.map(t => ([t.from.name, t.to.name])))
+  return uniq<Address>(names.map(n => AddressSchema.parse(n)))
 }
